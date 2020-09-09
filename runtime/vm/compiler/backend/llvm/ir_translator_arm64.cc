bc869edc8161d75af2dc7f717905ab5a93e5010d
linzj <zuojian.lzj@alibaba-inc.com>
2020-09-09 16:07:41 +0800

    Fix x23, x25 gets clobbered by write barriers.
    Clobber by runtime entry leaf calls.
    
    Change-Id: I42cc76976685bc604eb1945a5d8f6881b7c6b4eb

--- runtime/vm/compiler/backend/llvm/ir_translator_arm64.cc
+++ runtime/vm/compiler/backend/llvm/ir_translator_arm64.cc
@@ -52,7 +52,7 @@ LValue AnonImpl::LoadObjectFromPool(intptr_t offset) {
 
 const char* AnonImpl::GetSharedStubCSR() {
   return "x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,"
-         "x15,x18,x19,x20,x21,x22,x23,x24,x25,x26,x27,x28,q0,q1,"
+         "x15,x18,x19,x20,x21,x22,x24,x26,x27,x28,q0,q1,"
          "q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15,q16,"
          "q17,q18,q19,q20,q21,q22,q23,q24,q25,q26,q27,q28,q29,"
          "q30,q31";
@@ -65,7 +65,7 @@ const char* AnonImpl::GetCCallCSR() {
 
 const char* AnonImpl::GetBoxInt64SharedStubCSR() {
   return "x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15,x18,"
-         "x19,x20,x21,x22,x23,x24,x25,x26,x27,x28,q0,q1,q2,q3,"
+         "x19,x20,x21,x22,x24,x25,x26,x27,x28,q0,q1,q2,q3,"
          "q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15,q16,q17,q18,"
          "q19,q20,q21,q22,q23,q24,q25,q26,q27,q28,q29,q30,q31";
 }
